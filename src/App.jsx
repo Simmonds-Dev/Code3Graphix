f7f8abf74cb60c7c648c67dc4390e481586adcfe
Christoph551 <christophersimmonds551@gmail.com>
2025-04-28 17:39:21 -0700

    Initial file setup with navigation for Home, Products, Contact and Login

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="loginPage">
+            <h1>Login Information</h1>
+            <p>Learn how to pay for your custom engraving project.</p>
+        </div>
+    );
+};
+
+export default Login;
