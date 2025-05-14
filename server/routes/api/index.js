b60ffda8151ceb238cb0acacad02968006777908
Christoph551 <christophersimmonds551@gmail.com>
2025-05-13 18:17:40 -0700

    Saving login/signup functionality with JWT.

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import productRoutes from './productRoutes.js';
 // import categoryRoutes from './categoryRoutes.js';
-// import userRoutes from './userRoutes.js';
+import loginRoute from './loginRoute.js';
+import signupRoute from './signupRoute.js';
 // import orderRoutes from './orderRoutes.js';
 // import tagRoutes from './tagRoutes.js';
 
@@ -9,7 +10,8 @@ const router = express.Router();
 
 router.use('/products', productRoutes);
 // router.use('/categories', categoryRoutes);
-// router.use('/users', userRoutes);
+router.use('/login', loginRoute);
+router.use('/signup', signupRoute);
 // router.use('/orders', orderRoutes);
 // router.use('/tags', tagRoutes);
 
